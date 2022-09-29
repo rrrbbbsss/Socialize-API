55d2e66019baab0b5db32b4b7468587459b22f2a
Royce Strange <rrrbbbsss@ryzst.net>
2022-09-29 14:02:04 -0500

    start adding thought controls/routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,8 +2,44 @@ const { User, Thought } = require("../models");
 
 const ThoughtController = {
   getAll(req, res) {
-    res.json("todo");
+    Thought.find({})
+      .select("-__v")
+      .then((dbData) => res.json(dbData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json("there was an error");
+      });
   },
+  getOne(req, res) {
+    Thought.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((dbData) =>
+        !dbData
+          ? res.status(404).json({ message: "No user found with this id" })
+          : res.json(dbData)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json("there was an error");
+      });
+  },
+  // todo need to push this to associated user's thoughts array field
+  create(req, res) {
+    Thought.create(req.body)
+      .then((dbData) => res.json(dbData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json("there was an error");
+      });
+  },
+  // todo
+  update(req, res) {},
+  // todo
+  delete(req, res) {},
+  // todo
+  addReaction(req, res) {},
+  // todo
+  removeReaction(req, res) {},
 };
 
 module.exports = ThoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,7 +2,19 @@ const router = require("express").Router();
 const ThoughtController = require("../../controllers/thought-controller");
 
 // /api/thoughts
-router.route("/").get(ThoughtController.getAll);
-// todo rest of it...
+router.route("/").get(ThoughtController.getAll).post(ThoughtController.create);
+
+// /api/thoughts/<thought-id>
+router
+  .route("/:thoughtId")
+  .get(ThoughtController.getOne)
+  .put(ThoughtController.update)
+  .delete(ThoughtController.delete);
+
+// /api/thoughts/<thought-id>/reactions
+router
+  .route("/:thoughtId/reactions")
+  .post(ThoughtController.addReaction)
+  .delete(ThoughtController.removeReaction);
 
 module.exports = router;
