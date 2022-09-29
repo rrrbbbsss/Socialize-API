194baf1ff7caaff4e8b37d21c0b722fc1aad9ebd
Royce Strange <rrrbbbsss@ryzst.net>
2022-09-29 15:44:24 -0500

    tweak user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -54,14 +54,14 @@ const UserController = {
         res.status(400).json("there was an error");
       });
   },
-  // todo verify that deletes associated thoughts
   delete(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
       .then((dbData) => {
         !dbData
           ? res.status(404).json({ message: "No user found with this id" })
-          : // todo change the return message
-            res.json(dbData);
+          : Thought.deleteMany({ _id: dbData.thoughts }).then((dbData) =>
+              res.json("User and associated thoughts deleted")
+            );
       })
       .catch((err) => {
         console.log(err);
