ffedd63129362313891e061adc703758fc9f3909
Royce Strange <rrrbbbsss@ryzst.net>
2022-09-29 14:00:13 -0500

    add some basic seeds

---
+++ seeds/index.js
@@ -0,0 +1,25 @@
+const mongodb = require("../config/connection");
+const { User, Thought } = require("../models");
+const userSeeds = require("./user-seeds.json");
+const thoughtSeeds = require("./thought-seeds.json");
+
+mongodb.once("open", async () => {
+  try {
+    // clear out old entries
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    // seed users
+    await User.create(userSeeds);
+
+    // seed thoughts
+    // todo
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log("----------------");
+  console.log("Seeding Complete");
+  console.log("----------------");
+  process.exit(0);
+});
